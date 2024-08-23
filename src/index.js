156beb6005fc8ea98f5d02ec1d36bd4e718e1e7f
drago007-code <fastestway000@gmail.com>
2024-08-23 11:06:23 -0700

    good one

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ dotenv.config({
 })
 
 ConnectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000 ,()=>{
+        console.log(`server is running on port: ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log(`MongoDB connection Failed!!!`,error)
+})
