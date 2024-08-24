39b3028e090a64fca4c2c270950680f4395b419c
drago007-code <fastestway000@gmail.com>
2024-08-24 05:47:32 -0700

    next step now

---
+++ src/utils/Cloudnariy.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+
+// Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDNARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDNARY_API_KEY, 
+        api_secret: process.env.CLOUDNARY_API_SCRECT // Click 'View API Keys' above to copy your API secret
+    });
+    
+
+    const uploadOnCloudinary= async(localfilepath)=>{
+        try {
+            if (!localfilepath)return null 
+             const response=  await cloudinary.uploader.upload(localfilepath,{
+                resource_type:"auto"
+            })
+            console.log("file succcessfully uploaded", response.url);
+            return response;
+            }
+         catch (error) {
+            fs.unlinkSync(localfilepath)
+            return null
+            
+        }
+
+    }
